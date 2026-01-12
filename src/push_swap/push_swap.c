091e971b4211d25e5184412772cf3f12570b69bc
Sergii Sharmazanov <ssharmaz@c2r2p8.42vienna.com>
2026-01-12 16:02:54 +0100

    Add push_swap folder and Makefile

---
+++ src/push_swap/push_swap.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 23:17:00 by ssharmaz          #+#    #+#             */
+/*   Updated: 2026/01/12 15:54:38 by ssharmaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf/libftprintf.h"
+#include "../inc/ft_header.h"
+
+int	main(int ac, char **av)
+{
+	ft_fprintf(STDOUT, "There are %d arguments\n", ac);
+	(void)av;
+	return (0);
+}
